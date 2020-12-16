340db8b0742c77c570a9fedac8ab637ca9ed649a
Luke Browne <S00187306@mail.itsligo.ie>
2020-12-16 17:46:46 +0000

    Finished Facebook Login and Auth Guard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { OperatorListComponent } from './operators/operator-list/operator-list.component';
+import { AuthGuard } from './helpers/auth.guard';
+import { LoginComponent } from './login/login.component';
+import { CtuDetailsComponent } from './ctus/ctu-details/ctu-details.component';
+import { LoadoutDetailsComponent } from './loadouts/loadout-details/loadout-details.component';
 
 const routes: Routes = [ 
-  {path: "operator-list-component",
-   component: OperatorListComponent}
+  {path: 'operator-list-component', component: OperatorListComponent, canActivate: [AuthGuard]},
+  {path: 'ctu-list-component', component: CtuDetailsComponent},
+  {path: 'loadout-list-component', component: LoadoutDetailsComponent},
+  {path: 'login', component: LoginComponent}
  ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { APP_INITIALIZER, NgModule } from '@angular/core';
 
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { OperatorListComponent } from './operators/operator-list/operator-list.component';
 import { OperatorDetailsComponent } from './operators/operator-details/operator-details.component';
@@ -12,10 +13,14 @@ import { CtuListComponent } from './ctus/ctu-list/ctu-list.component';
 import { CtuDetailsComponent } from './ctus/ctu-details/ctu-details.component';
 import { CtuRowComponent } from './ctus/ctu-row/ctu-row.component';
 import { AppRoutingModule } from './app-routing.module';
-import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { OperatorFormComponent } from './operators/operator-form/operator-form.component';
+import { LoginComponent } from './login/login.component';
+import { ErrorInterceptor } from './helpers/error.interceptor';
+import { JwtInterceptor } from './helpers/jwt.interceptor';
+import { UserService } from './user.service';
+import { appInitializer } from './helpers/app.initializer';
 
 @NgModule({
   declarations: [
@@ -29,7 +34,8 @@ import { OperatorFormComponent } from './operators/operator-form/operator-form.c
     CtuListComponent,
     CtuDetailsComponent,
     CtuRowComponent,
-    OperatorFormComponent
+    OperatorFormComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
@@ -39,7 +45,11 @@ import { OperatorFormComponent } from './operators/operator-form/operator-form.c
     ReactiveFormsModule,
     NgbModule
   ],
-  providers: [],
+  providers: [
+    { provide: APP_INITIALIZER, useFactory: appInitializer, multi: true, deps: [UserService] },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
+     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },  
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
